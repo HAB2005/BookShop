6615d17937cb91ffa9d07503af5f7e10edac5a97
HAB2005 <hoanganhkhuat4869@gmail.com>
2025-12-09 17:44:56 +0700

    initial necessary file and folder

--- src/main/java/com/example/bookshop/BookshopApplication.java
+++ src/main/java/com/example/bookshop/BookshopApplication.java
@@ -2,8 +2,11 @@ package com.example.bookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.example.bookshop.config.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class BookshopApplication {
 
 	public static void main(String[] args) {
